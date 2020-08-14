f05fb4c69469d50b02aa2f8f65b0bb6eabc6b0e3
Rik <rik@huijzer.xyz>
2020-08-14 19:25:09 +0200

    Trigger CI (cache empty)

--- latex.nix
+++ latex.nix
@@ -3,7 +3,7 @@
 let
   inherit (nixpkgs) pkgs;
   myTex = with pkgs; texlive.combine {
-    inherit (texlive) scheme-basic pdflatex;
+    inherit (texlive) scheme-basic dvisvgm;
   };
 in [
   myTex
